c4c40b45683716fc891f224736b48d7506eb0c31
Benoit636 <benoitboudreau@vwmoncton.com>
2026-06-04 14:09:19 -0300

    Update tailwind.config.ts

--- fliplogic-web/tailwind.config.ts
+++ fliplogic-web/tailwind.config.ts
@@ -1,13 +1,15 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-content: [
-'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-],
-theme: {
-extend: {},
-},
-plugins: [],
+  content: [
+    './fliplogic-web/src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './fliplogic-web/src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './fliplogic-web/src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
+export default config
