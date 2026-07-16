61c7939486390e08dc94ddea6575162a17af7ce8
Benoit636 <benoitboudreau@vwmoncton.com>
2026-07-15 23:29:11 -0300

    Add debug alert

--- fliplogic-web/src/app/login/page.tsx
+++ fliplogic-web/src/app/login/page.tsx
@@ -12,6 +12,7 @@ export default function LoginPage() {
   const [loading, setLoading] = useState(false)
 
   const handleLogin = async () => {
+    alert('Button clicked!')
     setLoading(true)
     setError('')
     try {
