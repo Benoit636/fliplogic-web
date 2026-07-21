8f57fa16a289b4c928ca56f6586f85d1247508ce
Benoit636 <benoitboudreau@vwmoncton.com>
2026-07-20 21:12:58 -0300

    Add AuthProvider implementation

--- fliplogic-web/src/components/AuthProvider.tsx
+++ fliplogic-web/src/components/AuthProvider.tsx
@@ -1,1 +1,12 @@
+'use client';
 
+import { useEffect } from 'react';
+import { useAuthStore } from '@/store/auth';
+
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    useAuthStore.getState().hydrate();
+  }, []);
+
+  return <>{children}</>;
+}
